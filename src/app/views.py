1fa56b8830176fd2e414b8116474470b5727e2a0
David <davidforastier10@gmail.com>
2023-06-29 18:03:12 -0300

    Creamos el archivo login.html

--- src/app/views.py
+++ src/app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def home(request):
     template_name = 'home.html'
     return render(request, template_name, {})
+
+
+def login(request):
+    template_name = 'login.html'
+    return render(request, template_name, {})
