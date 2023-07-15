e74f1486f3469ee5ea605f1e5c71011b66f910a0
David <davidforastier10@gmail.com>
2023-07-15 14:23:43 -0300

    Recuperamos todos los registros y lo renderizamos en el html

--- src/eventos/views.py
+++ src/eventos/views.py
@@ -42,7 +42,16 @@ class ListarMisEventos(LoginRequiredMixin, ListView):
     model = MisEventos
     context_object_name = 'mis_eventos'
 
+
+    #Con esta función lo que estamos haciendo es filtrar todos los eventos en donde el id sea igual al del usuario logueado
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.filter(usuario_id=self.request.user.id)
         return queryset
+    
+    #Y con esta función lo que estamos haciendo es recuperar todos los registros del modelo 'eventos' y renderizarlo en el html
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        eventos = Eventos.objects.all()  # Recuperar todos los registros del modelo Eventos
+        context['eventos'] = eventos
+        return context
