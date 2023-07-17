2985a556cfc6e14b5c98519c3423ade002da66d4
David <davidforastier10@gmail.com>
2023-07-17 20:15:20 -0300

    Editar 'recursos'

--- src/recursos/urls.py
+++ src/recursos/urls.py
@@ -5,5 +5,6 @@ app_name = 'recursos'
 
 urlpatterns = [
     path('recursos/', views.ListarRecursos.as_view(), name='recursos' ),
-    path('crear_recurso/', views.CrearRecurso.as_view(), name='crear_recurso' )
+    path('crear_recurso/', views.CrearRecurso.as_view(), name='crear_recurso' ),
+    path('editar/<int:pk>', views.EditarRecurso.as_view() , name='editar')
 ]
--- src/recursos/views.py
+++ src/recursos/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from .models import Recursos
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .forms import RecursosForm
 from django.urls import reverse
 
@@ -15,6 +15,14 @@ class CrearRecurso(LoginRequiredMixin, CreateView):
     model = Recursos
     form_class = RecursosForm
     template_name = 'recursos/crear_recurso.html'
+
+    def get_success_url(self):
+        return reverse('recursos:recursos')
+    
+class EditarRecurso(LoginRequiredMixin, UpdateView):
+    template_name = 'recursos/editar_recurso.html'
+    model = Recursos
+    form_class = RecursosForm
 
     def get_success_url(self):
         return reverse('recursos:recursos')
