eff48a04a1c53fcdc4504ed68ca1b7170ab87aeb
David <davidforastier10@gmail.com>
2023-07-17 20:21:29 -0300

    Eliminar 'recursos'

--- src/recursos/urls.py
+++ src/recursos/urls.py
@@ -6,5 +6,6 @@ app_name = 'recursos'
 urlpatterns = [
     path('recursos/', views.ListarRecursos.as_view(), name='recursos' ),
     path('crear_recurso/', views.CrearRecurso.as_view(), name='crear_recurso' ),
-    path('editar/<int:pk>', views.EditarRecurso.as_view() , name='editar')
+    path('editar/<int:pk>', views.EditarRecurso.as_view() , name='editar'),
+    path('eliminar/<int:pk>', views.EliminarRecurso.as_view(), name='eliminar'),
 ]
--- src/recursos/views.py
+++ src/recursos/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from .models import Recursos
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import RecursosForm
 from django.urls import reverse
 
@@ -23,6 +23,14 @@ class EditarRecurso(LoginRequiredMixin, UpdateView):
     template_name = 'recursos/editar_recurso.html'
     model = Recursos
     form_class = RecursosForm
+
+    def get_success_url(self):
+        return reverse('recursos:recursos')
+    
+
+class EliminarRecurso(LoginRequiredMixin, DeleteView):
+    model = Recursos
+    template_name = 'recursos/eliminar_recurso.html'
 
     def get_success_url(self):
         return reverse('recursos:recursos')
