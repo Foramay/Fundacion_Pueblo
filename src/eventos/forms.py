748899687b9fa71e1c69b3acce260289741f22c7
David <davidforastier10@gmail.com>
2023-07-24 20:02:11 -0300

    Maquetado para los comentarios + estilo de bootstrap

--- src/eventos/forms.py
+++ src/eventos/forms.py
@@ -5,7 +5,9 @@ from django.utils.translation import gettext_lazy as _
 
 class EventosForm(forms.ModelForm):
     nombre=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese un nombre', 'style': 'height: 50px;'}))
-    descripcion=forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Ingrese la descripción', 'style': 'height: 100px;'}))
+    descripcion_corta=forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Ingrese la descripción corta', 'style': 'height: 100px;'}))
+    descripcion_completa=forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Ingrese la descripción completa', 'style': 'height: 100px;'}))
+
     imagen = forms.ImageField(widget=ClearableFileInput(attrs={'class': 'form-control-file'}), label=_('Imagen: '))
     class Meta:
         model = Eventos
