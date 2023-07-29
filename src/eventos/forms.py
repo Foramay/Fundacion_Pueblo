c6bcaf4cb19d204ee8f1650ec7cc4fbd9fe66be3
David <davidforastier10@gmail.com>
2023-07-29 15:07:42 -0300

    Campo 'modalidad'

--- src/eventos/forms.py
+++ src/eventos/forms.py
@@ -7,11 +7,11 @@ class EventosForm(forms.ModelForm):
     nombre=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese un nombre', 'style': 'height: 50px;'}))
     descripcion_corta=forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Ingrese la descripción corta', 'style': 'height: 100px;'}))
     descripcion_completa=forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Ingrese la descripción completa', 'style': 'height: 100px;'}))
-
+    modalidad = forms.ChoiceField(choices=Eventos.MODALIDAD_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
     imagen = forms.ImageField(widget=ClearableFileInput(attrs={'class': 'form-control-file'}), label=_('Imagen: '))
     class Meta:
         model = Eventos
-        fields = ['nombre', 'descripcion_corta', 'descripcion_completa', 'imagen']
+        fields = ['nombre', 'descripcion_corta', 'descripcion_completa', 'imagen', 'modalidad']
 
     def clean_nombre(self):
         nombre = self.cleaned_data['nombre']
--- src/eventos/models.py
+++ src/eventos/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from usuarios.models import Usuario
 
 
+
 class Eventos(models.Model):
+    MODALIDAD_CHOICES = (
+        ('V', 'Virtual'),
+        ('P', 'Presencial')
+    )
     nombre = models.CharField(max_length=30, null=False, blank=True)
     descripcion_corta = models.CharField(max_length=30)
     descripcion_completa = models.TextField(null=False, blank=True)
     fecha_evento = models.DateField(auto_now_add=True)
     imagen = models.ImageField(upload_to='imagenes_evento', null=True, blank=True)
-
+    modalidad = models.CharField(max_length=1, choices=MODALIDAD_CHOICES, default='P')
+    
     def __str__(self):
         return self.nombre
+
 class Comentario(models.Model):
     evento = models.ForeignKey(Eventos, on_delete=models.CASCADE)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
