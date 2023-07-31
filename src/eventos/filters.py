6ef1d97d07b64fc94d6883e5b8347ca56d6c7709
David <davidforastier10@gmail.com>
2023-07-31 12:02:37 -0300

    Personalizamos el html de filtrar

--- src/eventos/filters.py
+++ src/eventos/filters.py
@@ -1,7 +1,23 @@
 import django_filters
 from .models import Eventos
+from django import forms
 
 class FiltrarEvento(django_filters.FilterSet):
+    nombre = django_filters.CharFilter(
+        lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre del evento'})
+    )
+
+    MODALIDADES_CHOICES = (
+        ('V', 'Virtual'),
+        ('P', 'Presencial')
+    )
+    modalidad = django_filters.ChoiceFilter(
+        choices=MODALIDADES_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        empty_label='Seleccione una modalidad'
+    )
+
     class Meta:
         model = Eventos
-        fields = ['nombre', 'fecha_evento', 'modalidad']
+        fields = ['nombre', 'modalidad']
